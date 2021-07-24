96f776f1cd8ad8dd19ad4bd2a46bce37df7021c5
Alex.Lopez <alex.lopez.-nd@disney.com>
2021-07-24 09:01:58 -0500

    add auth model, mongo infra, error handler to auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { json } from 'body-parser'
 import 'express-async-errors'
+import mongoose from 'mongoose'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
@@ -16,12 +17,29 @@ app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-app.all('*', async () => {
-  throw new NotFoundError()
-})
+// app.all('*', async () => {
+//   throw new NotFoundError()
+// })
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('Listening 3000 !!');
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+
+  } catch (error) {
+    console.log(error);
+
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening 3000 !!');
+  })
+}
+
+start()
+
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,26 +1,32 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
 import { RequestValidationError } from '../errors/request-validation-error'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { BadRequestError } from '../errors/bad-request-error'
+import { User } from '../models/user'
 
 const router = express.Router()
 
 router.post('/api/users/signup', [
   body('email').isEmail().withMessage('Email must be provided'),
   body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 chars.')
-],(req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
     throw new RequestValidationError(errors.array())
-    // return res.status(400).send(errors.array())
   }
 
   const { email, password } = req.body
-  console.log('creating user...');
-  throw new DatabaseConnectionError()
+  const existingUser = await User.findOne({ email })
 
-  res.send('hi tehere signup')
+  if (existingUser) {
+    console.log('email in user');
+    throw new BadRequestError('Email in use')
+  }
+
+  const user = User.build({ email, password })
+  await user.save()
+  res.status(201).send(user)
 })
 
 export { router as signupRouter }
