0906f84a241fb3e8899eef2b63d28ee0dde1d266
Alex.Lopez <alex.lopez.-nd@disney.com>
2021-07-25 07:32:01 -0500

    add currenuser and requireauth middlewares

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,20 +1,11 @@
 import express from 'express'
 import { verify } from 'jsonwebtoken'
+import { currentUser } from '../middlewares/current-user'
 
 const router = express.Router()
 
-router.get('/api/users/currentUser', (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null })
-  }
-
-  try {
-    const payload = verify(req.session.jwt, process.env.JWT_KEY!)
-
-    res.send({ currentUser: payload })
-  } catch (error) {
-    res.send({ currentUser: null })
-  }
+router.get('/api/users/currentUser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null })
 })
 
 export { router as currentUserRouter }
