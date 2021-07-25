188425d96c40bfcf36e2d7a7096c55d81e5dca4c
Alex.Lopez <alex.lopez.-nd@disney.com>
2021-07-25 14:14:31 -0500

    client routes for auth

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,8 +3,8 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     const url = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
     return axios.create({
-      baseURL: url
-      headers: req.headers
+      baseURL: url,
+      headers: req.headers,
     })
   } else {
     return axios.create({
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client'
+import Header from '../components/header'
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
-export default App
+AppComponent.getInitialProps = async (appContext) => {
+  const api = buildClient({ req: appContext.ctx.req })
+  const { data } = await api.get('/api/users/currentuser')
+
+  let pageProps = {}
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+  }
+
+  return {
+    pageProps,
+    currentUser: data.currentUser
+  }
+}
+
+export default AppComponent
